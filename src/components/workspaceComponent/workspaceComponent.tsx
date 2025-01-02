6d83f375dfcde35b896ce36896b9b4c668cf989b
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-03 00:14:19 +0100

    latest commit

---
+++ src/components/workspaceComponent/workspaceComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WorkspaceComponent () {
+    return(
+        <div className="w-full h-40 p-32 bg-pink-700 shadow-lg">
+
+        </div>
+    )
+}
