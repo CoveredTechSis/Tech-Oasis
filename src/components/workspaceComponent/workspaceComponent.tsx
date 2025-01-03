ea26c35299dedc9dee976f3bb9c1e2e6509b479f
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-03 23:58:32 +0100

    latest commit

--- src/components/workspaceComponent/workspaceComponent.tsx
+++ src/components/workspaceComponent/workspaceComponent.tsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function WorkspaceComponent () {
     return(
         <div className="w-full h-40 p-32 bg-pink-700 shadow-lg">
-            <div className="flex w-fit justify-between">
+            <div className="flex w-4/5 justify-between">
                 <div className="text-center p-4 text-gray-50">
                    <h2 className="text-lg sm:text-sm">Transform your desk setup with beautiful tech accessories </h2>
                    <p className="text-lg sm:text-sm">Get all fancy aesthetics to make your space more inspirational for work. </p>
                 </div>
-               <div>
+               <div className="">
                <img src="/image/battlestation-desk-removebg.png" 
                
                alt="" />
