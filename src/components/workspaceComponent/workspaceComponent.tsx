5e42fe95737112bcd275cb04cbb3f22a972392f1
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-04 00:24:26 +0100

    latest commit

--- src/components/workspaceComponent/workspaceComponent.tsx
+++ src/components/workspaceComponent/workspaceComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function WorkspaceComponent () {
     return(
         <div className="w-full h-40 p-32 bg-pink-700 shadow-lg">
-            <div className="flex w-4/5 justify-between">
+            <div className="gap-4 mx-auto w-4/5 grid grid-cols-2 justify-between">
                 <div className="text-center p-4 text-gray-50">
                    <h2 className="text-lg sm:text-sm">Transform your desk setup with beautiful tech accessories </h2>
                    <p className="text-lg sm:text-sm">Get all fancy aesthetics to make your space more inspirational for work. </p>
