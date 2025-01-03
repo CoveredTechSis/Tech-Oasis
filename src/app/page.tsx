88722b942dd8ebf334864dc24dfc712aaf81413a
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-03 23:32:35 +0100

    latest commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       <BestSeller/>
       <FeaturedProduct/>
       <WorkspaceComponent/>
-
        </div>
   
   )
--- src/components/workspaceComponent/workspaceComponent.tsx
+++ src/components/workspaceComponent/workspaceComponent.tsx
@@ -3,6 +3,20 @@ import React from "react";
 export default function WorkspaceComponent () {
     return(
         <div className="w-full h-40 p-32 bg-pink-700 shadow-lg">
+            <div className="flex w-96 justify-between">
+                <div className="text-center p-4 text-gray-50">
+                   <h2>Transform your desk setup with beautiful tech accessories </h2>
+                   <p>Get all fancy aesthetics to make your space more inspirational for work. </p>
+                </div>
+               <div>
+               <img src="/image/desk-setup-aesthetic.jpg" 
+                width={200} 
+                height={200}
+               alt="" />
+
+               </div>
+
+            </div>
 
         </div>
     )
