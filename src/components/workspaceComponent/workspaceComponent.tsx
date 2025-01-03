40fc3235c86da56fe24f4f76eb7bd56dc30616ca
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-03 23:48:14 +0100

    latest commit

--- src/components/workspaceComponent/workspaceComponent.tsx
+++ src/components/workspaceComponent/workspaceComponent.tsx
@@ -5,11 +5,11 @@ export default function WorkspaceComponent () {
         <div className="w-full h-40 p-32 bg-pink-700 shadow-lg">
             <div className="flex w-fit justify-between">
                 <div className="text-center p-4 text-gray-50">
-                   <h2>Transform your desk setup with beautiful tech accessories </h2>
-                   <p>Get all fancy aesthetics to make your space more inspirational for work. </p>
+                   <h2 className="text-lg sm:text-sm">Transform your desk setup with beautiful tech accessories </h2>
+                   <p className="text-lg sm:text-sm">Get all fancy aesthetics to make your space more inspirational for work. </p>
                 </div>
                <div>
-               <img src="/image/desk-setup-aesthetic.jpg" 
+               <img src="/image/battlestation-desk-removebg.png" 
                
                alt="" />
 
