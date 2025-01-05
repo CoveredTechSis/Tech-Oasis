64e2a66c4a38b8db08fe02222a385daee9783f33
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-05 23:15:55 +0100

    latest commit

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,8 +2,15 @@ import React from "react"
 
 export default function Footer() {
     return(
-        <div>
-            
+        <div className="bg-purple-800 h-1/4 w-full ">
+            <div className="flex flex-col p-4">
+                <div>Shop</div>
+                <div>Company</div>
+                <div> Contact</div>
+                <div>Support</div>
+
+            </div>
+
         </div>
     )
 }
