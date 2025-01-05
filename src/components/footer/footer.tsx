4f292ce9ee2cf195b133bbfde73e7564addc6399
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-05 23:18:13 +0100

    latest commit

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
     return(
         <div className="bg-purple-800 h-1/4 w-full ">
             <div className="flex flex-col p-4">
-                <div>Shop</div>
-                <div>Company</div>
-                <div> Contact</div>
-                <div>Support</div>
+                <div className="text-white">Shop</div>
+                <div className="text-white">Company</div>
+                <div className="text-white"> Contact</div>
+                <div className="text-white">Support</div>
 
             </div>
 
