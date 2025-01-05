bc18d03e1ab7250193c04e4c8b136852027af44f
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-05 23:21:07 +0100

    latest commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HeroPage from "../components/heropage/heropage";
 import BestSeller from "../components/bestSeller/bestSeller";
 import FeaturedProduct from "@/components/featuredProducts/featuredProducts";
 import WorkspaceComponent from "@/components/workspaceComponent/workspaceComponent";
+import Footer from "@/components/footer/footer";
 // import ProductList from "./products/page";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <BestSeller/>
       <FeaturedProduct/>
       <WorkspaceComponent/>
+      <Footer/>
        </div>
   
   )
