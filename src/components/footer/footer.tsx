87616b040fa25ac19a01eabf8dadae752fba9290
CoveredTechSis <aishaolalekan@gmail.com>
2025-01-05 23:35:45 +0100

    latest commit

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Footer() {
     return(
-        <div className="bg-slate-300 h-20 w-full  ">
-            <div className="flex flex-row gap-10 justify-center align-middle p-4">
+        <div className="bg-slate-300 h-40 w-full  ">
+            <div className="flex flex-row gap-x-32 justify-center align-middle p-4">
                 <div className="text-black">Shop</div>
                 <div className="text-black">Company</div>
                 <div className="text-black"> Contact</div>
