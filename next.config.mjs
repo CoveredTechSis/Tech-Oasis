7c157e09dc52f8dc8c81a0f3d0591c658400a2a6
CoveredTechSis <aishaolalekan@gmail.com>
2025-06-30 14:18:51 +0200

    latest commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// next.config.js
+
+module.exports = {
+    images: {
+      domains: ['i.ytimg.com'], // ✅ Add your image host domain here
+    },
+  };
+  
 
 export default nextConfig;
